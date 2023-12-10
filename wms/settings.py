2f264bf1ea597ca0fb33f8b95b5363dc4ecdb376
strayman225 <67448975+strayman225@users.noreply.github.com>
2023-12-10 21:20:11 +0800

    Update settings.py

--- wms/settings.py
+++ wms/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-  os.path.join(BASE_DIR,"warehouse/static"),
+  os.path.join(BASE_DIR,"staticfiles"),
     
 ]
 
